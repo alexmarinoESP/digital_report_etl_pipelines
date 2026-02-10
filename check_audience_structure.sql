ea31eae425d9ba7d9c41910de0c41f9edcd453a7
Alessandro Benelli <alessandro.benelli@webfarm.com>
2026-02-10 15:50:25 +0100

    feat: cleaned project

--- check_audience_structure.sql
+++
@@ -1,7 +0,0 @@
-SELECT column_name, data_type, is_nullable 
-FROM columns 
-WHERE table_schema = 'GoogleAnalytics' 
-AND table_name = 'fb_ads_audience_adset'
-ORDER BY ordinal_position;
-
-SELECT * FROM GoogleAnalytics.fb_ads_audience_adset LIMIT 5;
--- check_custom_conversion.sql
+++
@@ -1,6 +0,0 @@
-SELECT COUNT(*) as total_rows 
-FROM GoogleAnalytics.fb_ads_custom_conversion_TEST;
-
-SELECT * 
-FROM GoogleAnalytics.fb_ads_custom_conversion_TEST 
-LIMIT 5;
